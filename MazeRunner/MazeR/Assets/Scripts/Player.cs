9544ce203e4cffa0f74091e296cf3ad07f219655
Ivette <car16020@uvg.edu.gt>
2017-05-14 17:41:16 -0600

    MazeRunner

---
+++ MazeRunner/MazeR/Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	private MazeCell currentCell;
+
+	private MazeDirection currentDirection;
+
+	public void SetLocation (MazeCell cell) {
+		if (currentCell != null) {
+			currentCell.OnPlayerExited();
+		}
+		currentCell = cell;
+		transform.localPosition = cell.transform.localPosition;
+		currentCell.OnPlayerEntered();
+	}
+
+	private void Move (MazeDirection direction) {
+		MazeCellEdge edge = currentCell.GetEdge(direction);
+		if (edge is MazePassage) {
+			SetLocation(edge.otherCell);
+		}
+	}
+
+	private void Look (MazeDirection direction) {
+		transform.localRotation = direction.ToRotation();
+		currentDirection = direction;
+	}
+
+	void Update()
+	{
+		/*
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+		*/
+
+		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+			Move(currentDirection);
+		}
+		else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+			Move(currentDirection.GetNextClockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+			Move(currentDirection.GetOpposite());
+		}
+		else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+			Move(currentDirection.GetNextCounterclockwise());
+		}
+
+		/*
+		var x = Input.GetAxis("Horizontal");
+		var z = Input.GetAxis("Vertical");
+		*/
+
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			Look(currentDirection.GetNextCounterclockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.E)) {
+			Look(currentDirection.GetNextClockwise());
+		}
+	}
+
+	/*
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent().material.color = Color.blue;
+	}
+	*/
+}
